7faa9a2e0c0a3239fb0580992f9ca3a3588eb03f
Pruthviraj49 <pdsuryawanshi45@gmail.com>
2023-06-28 23:53:21 +0530

    QR Scanner Added

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
           stream: FirebaseAuth.instance.authStateChanges(),
           builder: (context, snapshot) {
             if (snapshot.hasData) {
-              return const HomeScreen();
+              return const QRScanner();
             } else {
               return const LoginForm();
             }
--- lib/qrScanner.dart
+++ lib/qrScanner.dart
@@ -1,42 +1,128 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
-class QRScanner extends StatelessWidget {
+class QRScanner extends StatefulWidget {
   const QRScanner({super.key});
+
+  @override
+  State<QRScanner> createState() => _QRScannerState();
+}
+
+class _QRScannerState extends State<QRScanner> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: "QR");
+
+  Barcode? barcode;
+  QRViewController? controller;
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  void reassemble() async {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      await controller!.pauseCamera();
+    }
+    controller!.resumeCamera();
+  }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0.0,
-        ),
-        body: Container(
-            width: double.infinity,
-            child: Column(
-              children: [
-                Expanded(
-                    child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      "Place the QR Code in the area",
-                      style: TextStyle(color: Colors.black, fontSize: 16),
-                    ),
-                  ],
-                )),
-                Expanded(
-                    flex: 4,
-                    child: Container(
-                      color: Colors.white,
-                    )),
-                Expanded(
-                    child: Container(
-                  color: Colors.black,
-                )),
-              ],
-            )),
-      ),
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0.0,
+          ),
+          body: Stack(
+            alignment: Alignment.center,
+            children: [
+              buildQrView(context),
+              Positioned(bottom: 10, child: buildResult()),
+              Positioned(top: 10, child: builControlButtons()),
+            ],
+          )),
     );
   }
+
+  Widget buildQrView(BuildContext context) {
+    return (QRView(
+      key: qrKey,
+      onQRViewCreated: onQRViewCreated,
+      overlay: QrScannerOverlayShape(
+        borderColor: Theme.of(context).primaryColor,
+        borderLength: 20,
+        borderRadius: 10,
+        borderWidth: 10,
+        cutOutSize: MediaQuery.of(context).size.width * 0.8,
+      ),
+    ));
+  }
+
+  void onQRViewCreated(QRViewController controller) {
+    setState(() {
+      this.controller = controller;
+    });
+    controller.scannedDataStream.listen((barcode) {
+      setState(() {
+        this.barcode = barcode;
+      });
+    });
+  }
+
+  Widget buildResult() => Text(
+        barcode != null ? 'Result : ${barcode!.code}' : 'Scan QR Code',
+        maxLines: 3,
+        style: TextStyle(
+          fontSize: 20,
+          color: Colors.white,
+        ),
+      );
+
+  Widget builControlButtons() => Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          IconButton(
+            color: Colors.white,
+            icon: FutureBuilder(
+              future: controller?.getFlashStatus(),
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (snapshot.data != null) {
+                  return Icon(
+                      snapshot.data! ? Icons.flash_on : Icons.flash_off);
+                } else {
+                  return Container();
+                }
+              },
+            ),
+            onPressed: () async {
+              await controller?.toggleFlash();
+              setState(() {});
+            },
+          ),
+          IconButton(
+            color: Colors.white,
+            onPressed: () async {
+              await controller?.flipCamera();
+              setState(() {});
+            },
+            icon: FutureBuilder(
+              future: controller?.getCameraInfo(),
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (snapshot.data != null) {
+                  return Icon(Icons.switch_camera_outlined);
+                } else {
+                  return Container();
+                }
+              },
+            ),
+          )
+        ],
+      );
 }
